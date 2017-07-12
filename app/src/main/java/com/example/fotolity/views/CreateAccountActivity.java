9161f10c6afc62e6d181378df943ff2a0e1a81ac
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-07-11 22:05:08 -0500

    Maquetando la Actividad Login
    Se ha importado la librería de Design a nuestro proyecto Android y se ha hecho uso del control TextInputLayout y TextInputLayoutEditText para la creación de la pantalla de login de nuestra aplicación.

---
+++ app/src/main/java/com/example/fotolity/views/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.example.fotolity.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fotolity.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
