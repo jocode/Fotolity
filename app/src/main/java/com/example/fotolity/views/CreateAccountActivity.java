4dd562a3aae3465111d1da71d5b902bfbd42d523
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-07-12 21:49:34 -0500

    Creación de layout Create Account
    Se ha creado el diseño de la pantalla create account, hemos colocado un Elemento Toolbar y un scrollview con TextInputLayout para registrar un nuevo usuario. Se ha creado un método en la actividad CreateAccount donde definimos el Toolbar, este metodo recibe el título del toolbar y un booleano que establece un botón de vuelta en nuestra navegación de pantallas. El Toolbar se ha definido en un archivo aparte para hacer reutilización de código

--- app/src/main/java/com/example/fotolity/views/CreateAccountActivity.java
+++ app/src/main/java/com/example/fotolity/views/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.example.fotolity.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.example.fotolity.R;
 
@@ -11,5 +12,21 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
+        // Obtenemos el nombre del Toolbar parala vista
+        showToolbar(getResources().getString(R.string.toolbar_title_createaccount), false);
+
     }
+
+
+    public void showToolbar(String title, boolean upButton){
+        /**
+         * Es importante colocar estas dos lineas, para que el toolbar tenga soporte en versiones anteriores a Lollipop
+         */
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+    }
+
 }
