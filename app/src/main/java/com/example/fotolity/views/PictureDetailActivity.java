4071b02f0bbb33ddd595b6ee0de7399e1f784592
Johan Camilo Mosquera Ninco <joncamilomoni@gmail.com>
2017-07-17 19:23:33 -0500

    Se han hecho algunas modificaciones
    Se le han realizado algunas modificaciones a la aplicaciónen general, como el de destruir la actividad al dar clic en el Up Button del Toolbar

--- app/src/main/java/com/example/fotolity/views/PictureDetailActivity.java
+++ app/src/main/java/com/example/fotolity/views/PictureDetailActivity.java
@@ -1,9 +1,12 @@
 package com.example.fotolity.views;
 
+import android.os.Build;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Fade;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.example.fotolity.R;
@@ -16,6 +19,28 @@ public class PictureDetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_picture_detail);
 
         showToolbar("", true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Fade fade = new Fade();
+            fade.setDuration(300);
+            getWindow().setEnterTransition(fade);
+        }
+
+    }
+
+
+    /* Con este método destruimos la actividad actual, al dar clic en el botón Up de la toolbar, en lugar
+    * de iniciar de nuevo la actividad
+    */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void showToolbar(String title, boolean upButton) {
